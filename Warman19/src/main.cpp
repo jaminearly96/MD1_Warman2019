19d00002eaddbb6fc455b85bb174b85127c4d3f7
Jamin <jaminearly@gmail.com>
2019-09-14 14:20:14 +1000

    Moved motor control to the main

--- Warman19/src/main.cpp
+++ Warman19/src/main.cpp
@@ -1,44 +1,144 @@
 #include <Arduino.h>
-#include <motorController.h>
+#include <L298N.h>
 
-motorController speedyboi;
-int speed = 0.5;
+#define Motor1Pin1 11
+#define Motor1Pin2 12
+#define Motor1PinPWM 10
+#define Motor2Pin1 8
+#define Motor2Pin2 9
+#define Motor2PinPWM 7
+#define Motor3Pin1 5
+#define Motor3Pin2 6
+#define Motor3PinPWM 4
+#define Motor4Pin1 2
+#define Motor4Pin2 1
+#define Motor4PinPWM 3
+
+float speed = 0.5;
+
+// Motor Declaration
+L298N motor1(Motor1PinPWM, Motor1Pin1, Motor1Pin2);
+L298N motor2(Motor2PinPWM, Motor2Pin1, Motor2Pin2);
+L298N motor3(Motor3PinPWM, Motor3Pin1, Motor3Pin2);
+L298N motor4(Motor4PinPWM, Motor4Pin1, Motor4Pin2);
+
+// Sets the speed of the motors
+void setSpeed(float speed)
+{
+  motor1.setSpeed(speed * 255);
+  motor2.setSpeed(speed * 255);
+  motor3.setSpeed(speed * 255);
+  motor4.setSpeed(speed * 255);
+}
+
+/**********************************************
+ * Directions are as follows
+ *      1 > Forward
+ *      2 > Backwards
+ *      3 > Left
+ *      4 > Right
+ * *********************************************/
+void move(int direction)
+{
+  switch (direction)
+  {
+  case 1:
+    motor1.stop();
+    motor2.forward();
+    motor3.stop();
+    motor4.forward();
+    break;
+  case 2:
+    motor1.stop();
+    motor2.backward();
+    motor3.stop();
+    motor4.backward();
+    break;
+  case 3:
+    motor1.backward();
+    motor2.stop();
+    motor3.backward();
+    motor4.stop();
+    break;
+  case 4:
+    motor1.forward();
+    motor2.stop();
+    motor3.forward();
+    motor4.stop();
+    break;
+  }
+}
+
+// Stops all motors. duh
+void stop()
+{
+  motor1.stop();
+  motor2.stop();
+  motor3.stop();
+  motor4.stop();
+}
+
+/**********************************************
+ * Directions are as follows
+ *      1 > Clockwise
+ *      2 > Anticlockwise
+ * *********************************************/
+void rotate(int direction)
+{
+  switch (direction)
+  {
+  case 1:
+    motor1.forward();
+    motor2.forward();
+    motor3.forward();
+    motor4.forward();
+    break;
+  case 2:
+    motor1.backward();
+    motor2.backward();
+    motor3.backward();
+    motor4.backward();
+  }
+}
+
+// Pickup Balls
+void pickup(){
+
+}
+
+// Dropoff Balls
+void dropoff(){
+
+}
 
 void setup()
 {
   // Start serial monitor
   Serial.begin(9600);
-
-  // Use this to modify the speed. Put in a value between 0 and 1.
-  speedyboi.setSpeed(speed);
-
-  // Use this to move along each 4 axis. Look in the motor controller library for directions
-  speedyboi.move(1);
-
-  // Writes the current speed info of the motor to the screen every tick. Breaks after 100 ticks. This is likely to be broken bc fk cpp 2d arrays lol
-  for (int ticks = 0; ticks >= 100; ticks += 10)
-  {
-    float **motorStates = speedyboi.getMovementState();
-    for (int i = 0; i < 3; ++i)
-    {
-      Serial.print("Moving: ");
-      Serial.print(motorStates[i][0]);
-      Serial.print(" Speed: ");
-      Serial.print(motorStates[i][1]);
-      Serial.println();
-    }
-    delay(10);
-    Serial.write(27);
-    Serial.print("[2J");
-    Serial.write(27);
-    Serial.print("[H");
-  }
-
-  // Stops the motors
-  speedyboi.stop();
+  setSpeed(speed);
+  delay(5000);
 }
 
 void loop()
 {
-  // dab
+  move(4);
+  delay(1500);
+  stop();
+  pickup();
+  delay(500);
+  move(1);
+  delay(1000);
+  stop();
+  pickup();
+  delay(800);
+  move(1);
+  delay(1000);
+  stop();
+  pickup();
+  delay(1000);
+  move(3);
+  delay(3000);
+  stop();
+  dropoff();
+  delay(100000);
 }
